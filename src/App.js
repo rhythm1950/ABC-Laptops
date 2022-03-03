078bd53db7254115e81c3a1bf474a60b482f598e
rhythm1950 <rhythmshahriar0@gmail.com>
2022-03-03 20:55:04 +0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { GlobalStyle } from "./globalStyles";
+import { lazy, Suspense } from "react";
+
+const Home = lazy(() => import("./Pages/Home"));
+const Header = lazy(() => import("./components/Header/index"));
+const Footer = lazy(() => import("./components/Footer/index"));
+const ScrollToTop = lazy(() => import("./components/ScrollToTop/index"));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Suspense fallback={null}>
+        <GlobalStyle />
+        {/* Hi There! */}
+        <ScrollToTop />
+        <Header />
+        <Home />
+        <Footer />
+      </Suspense>
+    </>
   );
 }
 
